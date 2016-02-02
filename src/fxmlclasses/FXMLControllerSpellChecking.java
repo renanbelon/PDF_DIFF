dc280f474428d36cd854cbeaad00b8b482dd7eb5
andreicovaciu <andrei_covaciu98@yahoo.com>
2016-02-03 00:01:45 +0200

    brb

--- src/fxmlclasses/FXMLControllerSpellChecking.java
+++ src/fxmlclasses/FXMLControllerSpellChecking.java
@@ -5,8 +5,13 @@
 
 package fxmlclasses;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 
+import javax.swing.*;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,8 +19,60 @@ import java.util.ResourceBundle;
  * Created by Covaciu on 02/02/2016.
  */
 public class FXMLControllerSpellChecking implements Initializable {
+
+    @FXML
+    private Button startButton;
+    @FXML
+    private Button exitButton;
+    @FXML
+    private Button resetButton;
+    @FXML
+    private Button chooseFileButton;
+    @FXML
+    private TextArea textArea;
+
+    private File spellFile;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        startButton.setText("START");
+        exitButton.setText("Exit");
+        resetButton.setText("Reset file");
+        chooseFileButton.setText("Select file");
+        textArea.setPromptText("The text will be displayed here with red words if are incorrect spelled.");
+        startButton.setDisable(true);
+    }
+
+    @FXML
+    public void chooseSpellFile() {
+        JFileChooser fc = new JFileChooser();
+        int returnVal = fc.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            spellFile = fc.getSelectedFile();
+            textArea.setPromptText("");
+            startButton.setDisable(false);
+        } else {
+            spellFile = null;
+            startButton.setDisable(true);
+            textArea.setPromptText("PLEASE SELECT THE FILE!!!!");
+        }
+    }
+
+    @FXML
+    public void exit() {
+        System.exit(-1);
+    }
+
+    @FXML
+    public void reset() {
+        spellFile = null;
+        textArea.setText("");
+        textArea.setPromptText("The text will be displayed here with red words if are incorrect spelled.");
+        startButton.setDisable(true);
+    }
+
+    @FXML
+    public void startSpellChecking() {
 
     }
 }
